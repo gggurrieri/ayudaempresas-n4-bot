aa785ab7ab778595f0e2d3fffefd8f472b003404
gggurrieri <105894812+gggurrieri@users.noreply.github.com>
2025-04-15 17:15:31 -0300

    Update main_webhook.py

--- main_webhook.py
+++ main_webhook.py
@@ -6,9 +6,9 @@ from telegram import Update, Bot
 from telegram.ext import Application, CommandHandler, ContextTypes
 from calificador_n4 import calificar_url_n4
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+TELEGRAM_TOKEN = os.getenv("7641381058:AAGY2TXG9ZOLfq5Rql3ai0Ff0Jak_hf2dHY")
+TELEGRAM_CHAT_ID = os.getenv("5171106537")
+WEBHOOK_URL = os.getenv("https://ayudaempresas-n4-bot.onrender.com")
 
 bot = Bot(token=TELEGRAM_TOKEN)
 application = Application.builder().token(TELEGRAM_TOKEN).build()
